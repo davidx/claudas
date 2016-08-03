9073fddeaa2b291e2a08f2cc9ae414ba799d45d6
David Andersen <davidx@gmail.com>
2016-08-02 20:21:49 -0700

    refactor

--- lib/elyan.rb
+++ lib/elyan.rb
@@ -41,31 +41,27 @@ module Elyan
     non_author_comments(pr).collect { |comment| comment if contains_plus_one?(comment[:body]) }.compact
   end
 
-  def collect_merge_candidates
+  def collect_merge_candidates(repo)
     mylogger = Logger['mylog']
-    merge_candidates={}
-    Config[:repos].each do |repo|
-      mylogger.debug "\tREPO: #{repo} Getting PR List"
-      pr_list = pr_list_for_repo(repo)
-      mylogger.debug "\tREPO: #{repo} #{pr_list.length} Open Pull Requests"
+    mylogger.debug "\tREPO: #{repo} Getting PR List"
+    pr_list = pr_list_for_repo(repo)
+    mylogger.debug "\tREPO: #{repo} #{pr_list.length} Open Pull Requests"
 
-      merge_candidates[repo]=[]
-      pr_list.each do |pr|
+    merge_candidates=[]
+    pr_list.each do |pr|
 
-        pr[:non_author_comments] = non_author_comments(pr)
-        pr[:reviews] = reviews(pr)
-        reviewers = pr[:reviews].collect { |r| r[:user][:login] }
-        mylogger.debug "\t\tPR ##{pr[:number]} total comments: [#{pr[:comments].length}]"
-        mylogger.debug "\t\tPR ##{pr[:number]} non_author_comments: [#{pr[:non_author_comments].length}]"
-        mylogger.debug "\t\tPR ##{pr[:number]} code reviews: [#{pr[:reviews].length}] #{reviewers.join(",")}"
+      pr[:non_author_comments] = non_author_comments(pr)
+      pr[:reviews] = reviews(pr)
+      reviewers = pr[:reviews].collect { |r| r[:user][:login] }
+      mylogger.debug "\t\tPR ##{pr[:number]} total comments: [#{pr[:comments].length}]"
+      mylogger.debug "\t\tPR ##{pr[:number]} non_author_comments: [#{pr[:non_author_comments].length}]"
+      mylogger.debug "\t\tPR ##{pr[:number]} code reviews: [#{pr[:reviews].length}] #{reviewers.join(",")}"
 
-        if pr[:reviews].length >= Config[:minimum_reviewers]
-          mylogger.debug "\t\tMERGE Candidate"
-
-          merge_candidates[repo].push(pr)
-        else
-          mylogger.debug "\t\tSKIP"
-        end
+      if pr[:reviews].length >= Config[:minimum_reviewers]
+        mylogger.debug "\t\tMERGE Candidate"
+        merge_candidates.push(pr)
+      else
+        mylogger.debug "\t\tSKIP"
       end
     end
     merge_candidates
@@ -78,9 +74,9 @@ module Elyan
     def self.run_build_step(name, command)
       mylogger = Logger['mylog']
       output = `#{command}`
+      # set build status
       unless $? == 0
         mylogger.debug "[ #{name} ] [ERROR] \"#{command}\" Failed #{output}"
-        #set build status
         return false
       else
         mylogger.debug "[ #{name} ] [OK] \"#{command}\""
@@ -98,51 +94,50 @@ module Elyan
       mylogger.debug "CONFIG: #{Config.to_json}"
       mylogger.debug "CONFIG: #{Config[:repos].length} repos loaded"
 
-      data = {}
-      mylogger.debug "Collecting Merge Candidates"
-      merge_candidates=collect_merge_candidates
-      # p merge_candidates
-      mylogger.debug "Processing Merge Candidates"
       mylogger.debug "Setting tmp_dir at #{TMP_DIR}"
       FileUtils.mkdir_p(TMP_DIR)
 
-      merge_candidates.each do |repo, pr_list|
-        pr_list.each do |pr|
+      data = {}
+      mylogger.debug "Collecting Merge Candidates"
+      Config[:repos].each do |repo|
+        merge_candidate_pr_list = collect_merge_candidates(repo)
+
+        merge_candidate_pr_list.each do |pr|
           mylogger.debug "PR ##{pr[:number]} BEGIN"
           build_dir = File.join(TMP_DIR, "#{repo.gsub(/\//, '_')}_#{pr[:number].to_s}")
+
           mylogger.debug "Cleaning build dir #{build_dir}"
           FileUtils.rm_rf(build_dir)
+          if  run_build_step("CLONE", "git clone -q git@github.com:#{repo} #{build_dir}") &&
+              run_build_step("MASTER", "cd #{build_dir} && git checkout -q master") &&
+              run_build_step("BRANCH", "cd #{build_dir} && git checkout -q -b integration_branch_#{pr.head.ref}") &&
+              run_build_step("MERGE", "cd #{build_dir} && git rebase --verbose #{pr.head.sha}") &&
+              run_build_step("BUILD", "cd #{build_dir} && make build") &&
+              run_build_step("TEST", "cd #{build_dir} && make test")
 
-          next unless run_build_step("CLONE", "git clone -q git@github.com:#{repo} #{build_dir}")
-          # TRY MERGE
-          next unless run_build_step("MASTER","cd #{build_dir} && git checkout -q master")
-          next unless run_build_step("BRANCH","cd #{build_dir} && git checkout -q -b integration_branch_#{pr.head.ref}")
-          next unless run_build_step("MERGE","cd #{build_dir} && git rebase --verbose #{pr.head.sha}")
 
-          # try BUILD
-          next unless run_build_step("BUILD","cd #{build_dir} && make build")
-          # try TEST
-          next unless run_build_step("TEST","cd #{build_dir} && make test")
-          mylogger.debug "OK Merge, Build, Test phases successful."
+              mylogger.debug "OK MERGE,BUILD,TEST phase successful."
 
-          mylogger.debug "MERGING Pull Request (#{repo}, #{pr[:number]}, commit_message = 'Robot Merge OK'"
-          unless Choice[:non_interactive]
-            unless (ask("[INTERACTIVE MODE] : MERGING Pull Request #{repo} PR ##{pr[:number]} \"#{pr[:title]}\" OK? (y/n)") == "y")
-              say "OK, Safely exiting without merging"
-              next
-            end
+              unless Choice[:non_interactive] == true
+                unless ask("[INTERACTIVE MODE] : MERGING Pull Request #{repo} PR ##{pr[:number]} \"#{pr[:title]}\" OK? (y/n)") == "y"
+                  say "OK, Safely exiting without merging"
+                  next
+                end
+              end
+              mylogger.debug "MERGING Pull Request (#{repo}, #{pr[:number]}, commit_message = 'Robot Merge OK'"
+
+              begin
+                  Octokit.merge_pull_request(repo, pr[:number], commit_message = 'Elyan GitRobot Merge', options = {})
+                  mylogger.debug "PR ##{pr[:number]} Merge OK"
+                rescue => e
+                  mylogger.debug "PR ##{pr[:number]} Merge FAILED #{e.inspect}"
+                  next
+                end
+              mylogger.debug "PR ##{pr[:number]} END"
+
           end
-          begin
-            Octokit.merge_pull_request(repo, pr[:number], commit_message = 'Robot Merge OK', options = {})
-          rescue => e
-            mylogger.debug "PR ##{pr[:number]} Merge FAILED"
-            next
-          end
-          mylogger.debug "PR ##{pr[:number]} Merge OK"
-          mylogger.debug "PR ##{pr[:number]} END"
         end
       end
-
     end
   end
 end
